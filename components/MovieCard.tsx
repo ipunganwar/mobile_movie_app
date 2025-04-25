dbcc0493282354614d1a2b837eda67b9e76eec7e
ipunganwar <ipung.anwar1@gmail.com>
2025-04-25 15:38:23 +0700

    fix: movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Touchable, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { icons } from "@/constants/icons";
 
 const MovieCard = ({
   id,
@@ -10,7 +11,7 @@ const MovieCard = ({
   release_date,
 }: Movie) => {
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -19,10 +20,28 @@ const MovieCard = ({
               : "https://placehold.co/600x400/1a1a1a/FFFFFF.png",
           }}
           className="w-full h-52 rounded-lg"
-          // resizeMode="cover"
+          resizeMode="cover"
         />
 
-        <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+          <Text className="text-xs font-medium text-light-300 uppercase mr-1">
+            Movie
+          </Text>
+        </View>
       </TouchableOpacity>
     </Link>
   );
